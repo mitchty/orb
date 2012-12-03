e7520c49564b72b1668b0206e866ca5dd15fffd0
Mitch Tishmack <mitch.tishmack@gmail.com>
2012-12-03 09:49:31 -0600

    Typo.

--- orb-install.pl
+++ orb-install.pl
@@ -26,7 +26,7 @@ use Env qw(HOME LDFLAGS CPPFLAGS);
 use File::Path qw(make_path);
 
 # Globals, run away
-my $base_prefix    = "$HOME"/.orb;
+my $base_prefix    = "$HOME/.orb";
 my $default_prefix = "$base_prefix/rubies";
 my $install_prefix = $default_prefix;
 my $cache_dir      = "$base_prefix/cache";
