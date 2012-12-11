3c8ab855fe90483a91a82b7afb69160f5302c179
Mitch Tishmack <mitch.tishmack@gmail.com>
2012-12-10 22:15:17 -0600

    Add some tests, and fixes random things that failed in bash/ksh in orb.sh. More to follow.

--- orb.sh
+++ orb.sh
@@ -11,57 +11,88 @@ orb_python_base=${orb_python_base:=$orb_base/pythons}
 
 ORB_VERBOSE=${ORB_VERBOSE:=N}
 
-function orb_ls_path { echo $PATH | sed -e 's/\:/ /g' | fmt -1 }
+# NOTE: the K&R brace style is DELIBERATE
+# ksh93 and below (well bash 3.whatever as well) think:
+# function_name() {
+# blah
+# }
+# is a syntax error. This is the reason shell programming is like trying to
+# fight in a boxing match with both arms chopped off. Just a flesh wound.or
+orb_ls_path()
+{
+  echo $PATH | sed -e 's/\:/ /g' | fmt -1
+}
 
-function orb_to_path {
+orb_to_path()
+{
   cat /dev/stdin | awk '!a[$0]++' | tr -s '\n' ':' | sed -e 's/\:$//'
 }
 
-function orb_shift_path { [[ -d "$1" ]] && echo $1; orb_ls_path }
+orb_shift_path()
+{
+  [[ -d "$1" ]] && echo $1; orb_ls_path
+}
 
-function orb_rm_path {
+orb_rm_path()
+{
   tmp=$(orb_ls_path | grep -v $1 | orb_to_path)
   eval "PATH=$tmp"
   eval "export PATH"
 }
 
-function orb_add_path {
+orb_add_path()
+{
   tmp=$(orb_shift_path "$1" | orb_to_path)
   eval "PATH=$tmp"
   eval "export PATH"
 }
 
-function orb_implode {
+orb_implode()
+{
   orb_implode_ruby; orb_implode_perl; orb_implode_python
   unset orb; unset opl; unset opy
   for var in $(set | egrep -a "^(orb|ORB)_" | awk -F= '{print $1}'); do
     unset $var
   done
-  for func in $(typeset -f | egrep '^orb_'| awk '{print $1}'); do
+  for func in $(typeset -f | egrep '^orb_'| tr '(){' ' ' | awk -F= '{print $1}'); do
     unset -f $func
   done
 }
 
-function orb_implode_ruby { orb_rm_path $orb_ruby_base }
-function orb_implode_perl { orb_rm_path $orb_perl_base }
-function orb_implode_python { orb_rm_path $orb_python_base }
+orb_implode_ruby()
+{
+  orb_rm_path $orb_ruby_base
+}
 
-function orb_add_bin {
+orb_implode_perl()
+{
+  orb_rm_path $orb_perl_base
+}
+
+orb_implode_python()
+{
+  orb_rm_path $orb_python_base
+}
+
+orb_add_bin()
+{
   dir=$1; shift; lang=$1
 
   bin="$dir/bin/$lang"
   if [[ -f $bin ]]; then
     orb_add_path "$dir/bin"
+    return 0
   else
     orb_echo "$bin not found!"
     return 1
   fi
 }
 
-function orb_rm_vm {
+orb_rm_vm()
+{
   name=$1; shift; lang=$1
   echo $name | grep 'system' > /dev/null 2>&1
-  if [[ $? -eq 0 ]]; then
+  if (( $? == 0 )); then
     echo "I can't remove the system ${lang} install, sorry"
     return
   fi
@@ -70,35 +101,64 @@ function orb_rm_vm {
   [[ -f $bin ]] && rm -fr "$dir/$name"
 }
 
-function orb_use_internal {
+orb_use_internal()
+{
   lang=$1; shift; name=$1
-  eval "orb_implode_$lang"
   echo $name | grep 'system' > /dev/null 2>&1
-  if [[ $? -eq 0 ]]; then
+  if (( $? == 0 )); then
     return
   else
+    eval "orb_implode_$lang"
     base=$(eval "orb_${lang}_basedir")
-    eval "orb_add_bin '$base/$name' '$lang'"
+    eval "orb_add_bin $base/$name $lang"
   fi
 }
 
-function orb_use_ruby { orb_use_internal 'ruby' $1 }
-function orb_use_perl { orb_use_internal 'perl' $1 }
-function orb_use_python { orb_use_internal 'python' $1 }
-function orb_echo { [[ ${ORB_VERBOSE} != 'N' ]] && echo $* }
+orb_use_ruby()
+{
+  orb_use_internal 'ruby' $1
+}
+
+orb_use_perl()
+{
+  orb_use_internal 'perl' $1
+}
+
+orb_use_python()
+{
+  orb_use_internal 'python' $1
+}
+
+orb_echo()
+{
+  [[ ${ORB_VERBOSE} != 'N' ]] && echo $*
+}
 
 # conveniences for eval
-function orb_ruby_basedir { echo $orb_ruby_base }
-function orb_perl_basedir { echo $orb_perl_base }
-function orb_python_basedir { echo $orb_python_base }
+orb_ruby_basedir()
+{
+  echo $orb_ruby_base
+}
 
-function orb_ls_internal {
+orb_perl_basedir()
+{
+  echo $orb_perl_base
+}
+
+orb_python_basedir()
+{
+  echo $orb_python_base
+}
+
+orb_ls_internal()
+{
   lang=$1
   echo $(cd $(eval "orb_${lang}_basedir") &&
          echo "system" $(ls -d *) | fmt -1 | grep -v '^\.' | fmt -1000)
 }
 
-function orb_pick {
+orb_pick()
+{
   lang=$1
   tmp=$PS3
   PS3="$lang?: "
@@ -115,22 +175,23 @@ function orb_pick {
   PS3=$tmp
 }
 
-function orb_pick_private {
+orb_pick_private()
+{
   lang=$1; shift
   action=unknown
   do_index=0
   use_all=1
 
   for param in $*; do
-    [[ $param = 'all' ]] && use_all=0
-    if [[ $param = 'do' || $param = 'ls' || $param = 'rm' ]]; then
+    [[ $param == 'all' ]] && use_all=0
+    if [[ $param == 'do' || $param == 'ls' || $param == 'rm' ]]; then
       action=$param; break
     fi
     (( do_index=do_index+1 ))
   done
 
-  if [[ $action = 'do' ]]; then
-    if [[ $use_all -eq 0 ]]; then
+  if [[ $action == 'do' ]]; then
+    if [ $use_all -eq 0 ]; then
       shift; shift
       for a in $(orb_ls_internal $lang); do
         orb_use_internal $lang $a
@@ -152,7 +213,7 @@ function orb_pick_private {
         eval $*
       done
     fi
-  elif [[ $action = 'rm' ]]; then
+  elif [[ $action == 'rm' ]]; then
     index=0
     until [ $index -eq $do_index ]; do
       (( index=index+1 )); shift
@@ -160,13 +221,24 @@ function orb_pick_private {
     for a in $*; do
       orb_rm_vm $a $lang
     done
-  elif [[ $action = 'ls' ]]; then
+  elif [[ $action == 'ls' ]]; then
     orb_ls_internal $lang
   else
     orb_pick $lang
   fi
 }
 
-function orb { orb_pick_private 'ruby' $* }
-function opl { orb_pick_private 'perl' $* }
-function opy { orb_pick_private 'python' $* }
+orb()
+{
+  orb_pick_private 'ruby' $*
+}
+
+opl()
+{
+  orb_pick_private 'perl' $*
+}
+
+opy()
+{
+  orb_pick_private 'python' $*
+}
---
+++ test/test_orb.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env sh
+#-*-mode: Shell-script; coding: utf-8;-*-
+
+# Try and make sure what we've written functions sanely.
+it_passes_ksh_shell_checks_ok() {
+  if [[ -f "$(which ksh > /dev/null 2>&1)" ]]; then
+    ksh -n orb.sh
+    assertEquals 0 $?
+  fi
+}
+
+it_passes_ksh93_shell_checks_ok() {
+  if [[ -f "$(which ksh93 > /dev/null 2>&1)" ]]; then
+    ksh93 -n orb.sh
+    assertEquals 0 $?
+  fi
+}
+
+it_passes_bash_shell_checks_ok() {
+  if [[ -f "$(which bash > /dev/null 2>&1)" ]]; then
+    bash -n orb.sh
+    assertEquals 0 $?
+  fi
+}
+
+it_passes_zsh_shell_checks_ok() {
+  if [[ -f "$(which zsh > /dev/null 2>&1)" ]]; then
+    zsh -n orb.sh
+    assertEquals 0 $?
+  fi
+}
+
+it_sources_ok() {
+  source ./orb.sh
+}
+
+it_removes_itself_ok() {
+  orb_implode
+}
+
+it_really_leaves_no_variables_behind() {
+  source ./orb.sh
+  orb_implode
+  var_count=$(set | egrep -a "^(orb|ORB)_" | wc -l)
+  assertEquals 0 $var_count
+}
+
+it_really_leaves_no_functions_behind() {
+  source ./orb.sh
+  orb_implode
+  func_count=$(typeset -f | egrep "^orb_" | wc -l)
+  assertEquals 0 $func_count
+}
+
+. ./helper.sh
