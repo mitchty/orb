fca48166eb269fc379c898a832685988007e592b
Mitch Tishmack <mitch.tishmack@gmail.com>
2012-12-13 14:26:33 -0600

    Add basic perl tests.

---
+++ test/test_orb_perl.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env sh
+#-*-mode: Shell-script; coding: utf-8;-*-
+echo "Running $(basename $0)"
+
+it_should_use_system_perl_by_default_if_present() {
+  set +e
+  if [[ -f "$(which perl)" ]]; then
+    system_perl=$(which perl)
+    . ./orb.sh
+    orb_use_perl system
+    orb_perl=$(which perl)
+    assertEquals "${system_perl}" "${orb_perl}"
+    orb_implode
+  else
+    echo "Warning: no system perl found."
+    assertEquals 0 0
+  fi
+}
+
+it_should_detect_perl_installs_to_orb_perl_base() {
+  set +e
+  orb_base=$(pwd)
+  export orb_base
+  . ./orb.sh
+  mock_perl=$orb_perl_base/default/bin/perl
+  mock_install "$orb_perl_base/default/perl"
+  assertEquals "$(opl ls)" "system default"
+  orb_implode
+}
+
+it_should_use_perl_installs_to_orb_perl_base() {
+  set +e
+  orb_base=$(pwd)
+  . ./orb.sh
+  mock_perl=$orb_perl_base/default/bin/perl
+  mock_install $mock_perl
+  orb_use_perl default
+  assertEquals "${mock_perl}" "$(which perl)"
+  orb_implode
+}
+
+it_should_not_use_directories_in_orb_base_with_no_perl_bin() {
+  set +e
+  orb_base=$(pwd)
+  system_perl=$(which perl)
+  . ./orb.sh
+  mkdir -p $orb_base/default/bin
+  touch $orb_base/default/bin/zomg
+  orb_use_perl default
+  assertEquals "${system_perl}" "$(which perl)"
+  orb_implode
+}
+
+it_should_not_use_nonexecutable_perl_in_orb_base() {
+  set +e
+  orb_base=$(pwd)
+  system_perl=$(which perl)
+  . ./orb.sh
+  mkdir -p $orb_base/default/bin
+  touch $orb_base/default/bin/perl
+  orb_use_perl default
+  assertEquals "${system_perl}" "$(which perl)"
+  orb_implode
+}
+
+. ./helper.sh
