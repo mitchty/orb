007001482a73480e71748cc0c15df4fbac70af50
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-05-20 18:42:13 -0500

    Jruby test for 1.7.4

--- test/smoketest_jruby.sh
+++ test/smoketest_jruby.sh
@@ -12,13 +12,13 @@ it_installs_latest_jruby() {
   export orb_base
   source ./orb.sh
   ./ruby-install --ruby=${engine} > /dev/null 2>&1
-  engine_version=1.7.3
+  engine_version=1.7.4
   orb use ${engine}-${engine_version}
   sandbox_ruby=$orb_ruby_base/${engine}-${engine_version}/bin/ruby
   ruby_ver_string=$(ruby -v | perl -pe 's/\son.*//g;')
   set -e
   assertEquals "$sandbox_ruby" "$(which ruby)"
-  assertEquals 'jruby 1.7.3 (1.9.3p385) 2013-02-21 dac429b' "$ruby_ver_string"
+  assertEquals 'jruby 1.7.4 (1.9.3p392) 2013-05-16 2390d3b' "$ruby_ver_string"
 }
 
 . ./helper.sh
