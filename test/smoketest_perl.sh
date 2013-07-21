58f604d23e20e008c8fa49bbb5c402667fdf5ccf
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-07-21 09:02:52 -0500

    Update smoketests to use orb/opl install.

--- test/smoketest_perl.sh
+++ test/smoketest_perl.sh
@@ -9,16 +9,22 @@ it_installs_latest_perl() {
   set +e
   engine=perl
   engine_version=${perl_version}
+  engine_name=${engine}-${engine_version}
   orb_base=$(pwd)
   export orb_base
+
   source ./orb.sh
-  ./perl-install --no-test --version=${engine_version} > /dev/null 2>&1
-  opl use ${engine}-${engine_version}
-  sandbox_perl=$orb_perl_base/${engine}-${engine_version}/bin/perl
+
+  opl install --no-test --version=${engine_version} > /dev/null 2>&1
+  opl use ${engine_name}
+
+  sandbox_perl=$orb_perl_base/${engine_name/bin/perl
   perl_ver_string=$(perl -v | perl -pe 's/\sbuilt\sfor\s.*//g;' | grep 'This is')
+
   set -e
   assertEquals "$sandbox_perl" "$(which perl)"
   assertEquals "${perl_verbose}" "$perl_ver_string"
+  assertEquals $(orb which) "${engine_name}"
 }
 
 . ./helper.sh
--- test/smoketest_ruby.sh
+++ test/smoketest_ruby.sh
@@ -9,18 +9,24 @@ it_installs_latest_ruby() {
   set +e
   engine=ruby
   engine_version=$ruby_version
+  engine_name=${engine}-${engine_version}
   # ruby -v omits the -
   engine_version_short=$(echo $engine_version | sed -e 's/\-//g')
   orb_base=$(pwd)
   export orb_base
+
   source ./orb.sh
-  ./ruby-install > /dev/null 2>&1
-  orb use ${engine}-${engine_version}
-  sandbox_ruby=$orb_ruby_base/${engine}-${engine_version}/bin/ruby
+
+  orb install --version=${engine_version} > /dev/null 2>&1
+  orb use ${engine_name}
+
+  sandbox_ruby=$orb_ruby_base/${engine_name}/bin/ruby
   ruby_ver_string=$(ruby -v | perl -pe 's/\s\[.*$//g')
+
   set -e
   assertEquals "$sandbox_ruby" "$(which ruby)"
   assertEquals "${engine} ${engine_version_short} ${ruby_verbose}" "$ruby_ver_string"
+  assertEquals $(orb which) "${engine_name}"
 }
 
 . ./helper.sh
