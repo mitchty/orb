bb0b21bf584bdb2e8ea3071586f54371e5df03c7
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-12-14 15:33:32 -0600

    Update tap test suite to fit current expectations.

--- Orb/Install_test.pl
+++ Orb/Install_test.pl
@@ -10,17 +10,33 @@ BEGIN {
   use_ok('Orb::Install');
 }
 
-isnt(Orb::Install::latest_perl_from_web, 'unknown', 'We can parse the latest perl release.');
-ok(Orb::Install::latest_perl_from_web, '5.18.0');
+isnt(Orb::Install::latest_perl_from_web, 'unknown',
+     'We can parse the latest perl release.');
 
-isnt(Orb::Install::latest_ruby_from_web, 'unknown', 'We can parse the latest ruby release.');
-ok(Orb::Install::latest_ruby_from_web, '2.0.0-p247');
+ok(Orb::Install::latest_perl_from_web eq '5.18.1',
+   "Latest perl version expected.");
 
-isnt(Orb::Install::latest_jruby_from_web, 'unknown', 'We can parse the latest jruby release.');
-ok(Orb::Install::latest_jruby_from_web, '1.7.4');
+ok(Orb::Install::latest_perl_from_web ne '5.18.0',
+     '5.18.0 isn\'t the latest perl');
 
-isnt(Orb::Install::latest_python_from_web, 'unknown', 'We can parse the latest python3 release.');
-ok(Orb::Install::latest_python_from_web, '3.3.2');
+isnt(Orb::Install::latest_ruby_from_web, 'unknown',
+     'We can parse the latest ruby release.');
+
+ok(Orb::Install::latest_ruby_from_web eq '2.0.0-p353',
+    'Latest version of ruby expected.');
+
+isnt(Orb::Install::latest_jruby_from_web, 'unknown',
+     'We can parse the latest jruby release.');
+
+ok(Orb::Install::latest_jruby_from_web eq '1.7.9',
+    'Latest version of ruby expected.');
 
 ok(Orb::Install::python_download_url('1.2.3') eq
-  'http://python.org/ftp/python/1.2.3/Python-1.2.3.tgz', 'python_download_url');
+  'http://python.org/ftp/python/1.2.3/Python-1.2.3.tgz',
+   'python_download_url');
+
+isnt(Orb::Install::latest_python_from_web, 'unknown',
+     'We can parse the latest python3 release.');
+
+ok(Orb::Install::latest_python_from_web eq '3.3.3',
+   'Latest version of python 3.0 expected');
