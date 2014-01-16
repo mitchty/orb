bb7a844e4f729ce27448cf0db67f7a7ad1fc46c9
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-01-15 20:38:44 -0600

    Remove old tests based on shunit2.

--- t/smoketest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-#-*-mode: Shell-script; coding: utf-8;-*-
-
-./test_all.sh
-
-for smoke in $(ls -d smoketest_*.sh | grep -v all | grep -v head); do
-  (./$smoke)
-done
--- t/test_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-#-*-mode: Shell-script; coding: utf-8;-*-
-
-for shtest in $(ls -d test_*.sh | grep -v all); do
-  (./$shtest)
-done
